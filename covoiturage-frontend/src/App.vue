2be5e99657280e11b5f5464f0aa69aa82229be9b
Sheerven <ayordletamer@gmail.com>
2024-03-21 15:48:08 +0100

    Creatoin branche

--- covoiturage-frontend/src/App.vue
+++ covoiturage-frontend/src/App.vue
@@ -5,14 +5,6 @@ import BarreDeNavigation from './components/BarreDeNavigation.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
     <TheWelcome />
   </main>
